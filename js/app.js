8e7b627cc14fccb10b5ccd4951b171e2597f94cc
CharlyWelch <blwbiology@gmail.com>
2017-11-07 14:07:55 -0800

    add chart.js to folder, build chart in code

--- js/app.js
+++ js/app.js
@@ -25,18 +25,6 @@ const wineGlass = new Product('wine glass', 'wine-glass.jpg');
 
 const products = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun, unicorn, usb, waterCan, wineGlass];
 
-// create arrays for name labels, number of times clicked, number of times displayed
-
-const prodNames = [];
-const clickedSet = [];
-const displayedSet = [];
-
-for (let i = 0; i < products.length; i++) {
-    prodNames.push(products[i].name);
-    clickedSet.push(products[i].clicked);
-    displayedSet.push(products[i].displayed);
-}
-
 // Put three random products on page, no duplicates in each three
 
 function appendRandomProduct () {
@@ -93,8 +81,45 @@ function clickHandler (e) {
 
 // function to remove event listener at 25 clicks, add data to page
 
+const prodNames = [];
+const clickedSet = [];
+const displayedSet = [];
+
 function endSurvey () {
     const select = document.getElementById('select');
     select.removeEventListener('click', clickHandler);
+
+    for (let i = 0; i < products.length; i++) {
+        prodNames.push(products[i].name);
+        clickedSet.push(products[i].clicked);
+        displayedSet.push(products[i].displayed);
+    }
 }
 
+// add chart.js chart using Canvas element:
+
+const chartCanvas = document.getElementById('chart');
+const chartCtx = chartCanvas.getContext('2d');
+
+const chart = new Chart(
+    chartCtx,
+    {
+        type: 'bar',
+        data: {
+            labels: prodNames,
+            datasets: [
+                {
+                    label:'Product Name',
+                    data: clickedSet,
+                    backgroundColor: 'rgba(255,100,20,1)',
+                }
+            ]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Products Chosen',
+            }
+        }
+    }
+);
